82b70100273c035cdbc5dac784299e12ce337deb
Shubham <shubhamk.khairnar@gmail.com>
2023-08-31 20:35:16 +0530

    new starting

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { OrdersComponent } from './orders/orders.component';
+import { SearchBoxDirective } from './search-box.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomerComponent,
+    OrdersComponent,
+    SearchBoxDirective
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
